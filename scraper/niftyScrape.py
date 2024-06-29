f56a3711ce24968d9e7796af5d931975d3723920
Ariyaman Debnath <ariyamandebnath.ad@gmail.com>
2024-06-29 14:14:27 +0530

    StartCreating models for all the data

--- scraper/niftyScrape.py
+++ scraper/niftyScrape.py
@@ -171,7 +171,7 @@ def stock_df(symbol, from_date, to_date, series="EQ"):
     return df
 
 if __name__ == "__main__":
-    symbol = 'HDFCBANK'
+    symbol = 'AXIS'
     from_date = datetime.strptime('01-01-2013', '%d-%m-%Y')
     to_date = datetime.strptime('27-06-2024', '%d-%m-%Y')
-    stock_csv(symbol, from_date, to_date, series="EQ", output="../data/historical_stock_data_HDFCBANK.csv", show_progress=True)
+    stock_csv(symbol, from_date, to_date, series="EQ", output="../data/historical_stock_data_AXIS.csv", show_progress=True)
