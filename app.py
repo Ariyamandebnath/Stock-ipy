08826064a928b49e6220e8668dba72f2346b46a7
Ariyaman Debnath <ariyamandebnath.ad@gmail.com>
2024-06-30 16:46:16 +0530

    initializing the app

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
-import numpy as np 
-import pandas as pd 
-import
+import numpy as np
+import pandas as pd
+import streamlit as st 
+from datetime import date
+from keras.models import load_model
+
+model = load_model('models/LSTM/StockPredictionModel_AXIS.keras')
+
+st.header("Stock Market Predictor")
+
+stock = st.selectbox("Select the Stock you'd like to predict on",("SBIN (State Bank of India)","HDFC (HDFC Bank)","AXIS (Axis Bank)"))
+
+SelectedModel =st.selectbox("Select the ml model you'd like to implement for the prediction :>",("LSTM (Long short-term memeory)","GRU (Gated Recurrent Unit)","ARIMA (Autoregressive integrated moving average)"))
